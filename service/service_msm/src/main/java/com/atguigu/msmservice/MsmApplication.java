852c69c7937a0be8c055ff539b7fdf62f8dc4364
liushangupup <976681935@qq.com>
2020-10-17 00:14:44 +0800

    整合JWT与阿里云短信服务，实现登陆与注册的后端接口

---
+++ service/service_msm/src/main/java/com/atguigu/msmservice/MsmApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.msmservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.atguigu"})
+public class MsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsmApplication.class, args);
+    }
+}
---
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/service/UcenterMemberService.java
@@ -0,0 +1,20 @@
+package com.atguigu.educenter.service;
+
+import com.atguigu.educenter.entity.UcenterMember;
+import com.atguigu.educenter.entity.vo.RegisterVo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 会员表 服务类
+ * </p>
+ *
+ * @author liushang
+ * @since 2020-10-16
+ */
+public interface UcenterMemberService extends IService<UcenterMember> {
+
+    String login(UcenterMember member);
+
+    void register(RegisterVo registerVo);
+}
